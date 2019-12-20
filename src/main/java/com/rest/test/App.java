1a1932ecdf180d1bdb2e0c29f75df468362ee5b7
Andy Zhang <zch.ustc@gmail.com>
2019-12-20 16:25:22 +0800

    enable prometheus metrics

--- src/main/java/com/rest/test/App.java
+++ src/main/java/com/rest/test/App.java
@@ -1,25 +1,29 @@
 package com.rest.test;
 
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.apache.commons.lang3.time.StopWatch;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 @Path("/")
 public class App extends Application {
     private final Keywords keywords;
+    private static Random rand = new Random();
+    private static PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
 
     public App() throws IOException {
         this.keywords = new Keywords("localhost", 6379);
@@ -41,27 +45,61 @@ public class App extends Application {
     @Path("/keywords")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listKeyword() {
-        return Response.ok(this.keywords.list()).build();
+    public Response listKeyword() throws InterruptedException {
+        StopWatch sw = new StopWatch();
+        sw.start();
+        try {
+            Thread.sleep(rand.nextInt(500));
+            return Response.ok(this.keywords.list()).build();
+        } finally {
+            registry.counter("hot_key_request_total", "method", "GET", "path", "/keywords", "status", "200").increment();
+            registry.summary("hot_key_request_duration", "method", "GET", "path", "/keywords", "status", "200").record(sw.getNanoTime() / Math.pow(10, 9));
+        }
     }
 
     @Path("/keywords/{keyword}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getKeyword(@PathParam("keyword") final String keyword) {
+    public Response getKeyword(@PathParam("keyword") final String keyword) throws InterruptedException {
         HotKeyword ret = this.keywords.get(keyword);
-        if( ret == null) {
-            return Response.status(Status.NOT_FOUND).build();
-        } else {
-            return Response.ok(ret).build();
+        StopWatch sw = new StopWatch();
+        sw.start();
+        Thread.sleep(rand.nextInt(500));
+        int status = 200;
+        try {
+            if (ret == null) {
+                status = 404;
+                return Response.status(Status.NOT_FOUND).build();
+            } else {
+                return Response.ok(ret).build();
+            }
+        } finally {
+            registry.counter("hot_key_request_total", "method", "GET", "path", "/keywords/{keyword}", "status", Integer.toString(status)).increment();
+            registry.summary("hot_key_request_duration", "method", "GET", "path", "/keywords/{keyword}", "status", Integer.toString(status)).record(sw.getNanoTime() / Math.pow(10, 9));
         }
     }
 
     @Path("/keywords/{keyword}")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addKeyword(@PathParam("keyword") final String keyword) {
-        return Response.ok(this.keywords.put(keyword)).build();
+    public Response addKeyword(@PathParam("keyword") final String keyword) throws InterruptedException {
+        StopWatch sw = new StopWatch();
+        sw.start();
+        Thread.sleep(rand.nextInt(500));
+        int status = rand.nextInt(10) > 8 ? 400 : 200;
+        try {
+            return Response.ok(this.keywords.put(keyword)).build();
+        } finally {
+            registry.counter("hot_key_request_total", "method", "POST", "path", "/keywords/{keyword}", "status", Integer.toString(status)).increment();
+            registry.summary("hot_key_request_duration", "method", "POST", "path", "/keywords/{keyword}", "status", Integer.toString(status)).record(sw.getNanoTime() / Math.pow(10, 9));
+        }
+    }
+
+    @Path("/metrics")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMetrics() {
+        return registry.scrape();
     }
 
     public static void main(String[] args) throws Exception {
@@ -83,6 +121,8 @@ public class App extends Application {
         // "com.irootech.mds.examples.metrics.jetty");
         // servlet.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
 
+        new JvmThreadMetrics().bindTo(registry);
+        new JvmGcMetrics().bindTo(registry);
 
         try {
             jettyServer.start();
